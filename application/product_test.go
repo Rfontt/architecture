0c97e928ae30af619ed1e84fcfb7eaf54c58e7ba
Rfontt <ritadecassia.infor2@gmail.com>
2024-02-15 19:25:33 -0300

    feat: add tests to IsValid method

--- application/product_test.go
+++ application/product_test.go
@@ -54,4 +54,14 @@ func TestProduct_IsValid(t *testing.T) {
 	_, err = product.IsValid()
 
 	require.Equal(t, "The state must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+
+	require.Equal(t, "The price must be greater than zero or equal zero", err.Error())
 }
