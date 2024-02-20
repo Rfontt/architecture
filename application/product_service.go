a223a8aaf31cf45f7bb13b220ddd64b53772f9e8
Rfontt <ritadecassia.infor2@gmail.com>
2024-02-20 19:41:14 -0300

    feat: add product service

---
+++ application/product_service.go
@@ -0,0 +1,13 @@
+type ProductService struct {
+	Persistence ProductPersistenceInterface
+}
+
+func (s *ProductService) Get(id String) (ProductInterface, error) {
+	product, err := s.Persistence.Get(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
